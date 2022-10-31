927fc2cda7efd74d075eb324c73b8046dff46cdb
Fatima <fatima.abdulmalik@hotmail.com>
2022-10-31 10:46:30 +0300

    Initial Commit

---
+++ signin/signinApp.swift
@@ -0,0 +1,17 @@
+//
+//  signinApp.swift
+//  signin
+//
+//  Created by Fatima Alismail on 06/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct signinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
